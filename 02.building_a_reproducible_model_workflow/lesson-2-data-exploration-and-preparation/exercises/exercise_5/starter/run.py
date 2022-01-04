e5518b939ce7a4ba1c6bc24e2ccaf31c009b4f86
Ward <award40@LAMU02WH4RCHTD6.uhc.com>
2022-01-04 19:18:33 +0000

    ADD - EX-5: Run.py code for preprocessing data and logging to WandB

--- 02.building_a_reproducible_model_workflow/lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ 02.building_a_reproducible_model_workflow/lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -13,8 +13,37 @@ def go(args):
 
     run = wandb.init(project="exercise_5", job_type="process_data")
 
-    ## YOUR CODE HERE
-    pass
+    ## YOUR CODE HERE #"exercise_4/genres_mod.parquet:latest"
+    artifact = run.use_artifact(args.input_artifact)
+    df = pd.read_parquet(artifact.file(), engine="pyarrow")
+
+    logger.info("dropping duplicate rows...")
+    df = df.drop_duplicates().reset_index(drop=True)
+
+    """
+    NOTE: again, in a real setting, you will have to make sure that your feature store
+    provides this text_feature at inference time, OR, you will have to move
+    the computation of this feature to the inference pipeline.
+    """
+    logger.info("performing feature engineering...")
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+
+    file_name = 'preprocessed_data.csv'
+    df.to_csv(file_name)
+
+
+    logger.info("Creating artifact for dataframe..")
+    artifact = wandb.Artifact(
+            name=args.artifact_name,
+            type=args.artifact_type,
+            description=args.artifact_description
+        )
+    artifact.add_file(file_name)
+
+    logger.info("Logging artifact")
+    run.log_artifact(artifact)
 
 
 if __name__ == "__main__":
